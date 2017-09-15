c745bd7b090f86746cfe0a06d94e6fc4691a3e12
Federico Pizzolo <federico.pizzolo2@enel.com>
2017-09-15 17:42:02 +0200

    started working on server

--- server/jIP_server.sh
+++ server/jIP_server.sh
@@ -1,15 +1,40 @@
 CLIENT_PATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 function acquireRequests {
-# check requests folder for incoming JSONs, process one by one
-  folder='requests'
-  for $i.json in $folder:
-    key = "key"
-    validateKey $key
-    processRequest $i.json
+# periodic check: JSONs available inside requests folder?
+# if available, send for processing
+  folder=$CLIENT_PATH/requests
+  file=request*.json
+  orderbook=$folder/orderbook.json # orderbook
+  pending=$(ls -1 $folder/$file | wc -l) # let's check how many requests we got
+  echo '[' > $orderbook
+  for i in $(echo $folder/$file); do
+    #TODO: maybe check request key against an accepted dictionary? Although scp already checks sender identity
+    pending=$[$pending-1] # decrement requests remaining
+    parseJSON "$i"| buildOrderbook $orderbook $pending # parse i-th request and append to orderbook
+  done
+  removeOld
 }
 
-function processRequest {
+function parseJSON {
+  # Use jq to parse a pending request
+  input=$(cat $1)
+  output=$(echo "$input" | jq .[])
+  echo $output
+}
+
+function buildOrderbook {
+  # The idea is to add json blocks to an orderbook file (first argument).
+  # To provide proper syntax, a "remaining block" counter (second argument, $pending) is provided.
+  # While $pending>0, a ",\n" is written after the block
+  # Write "\n]" then reparse to adjust syntax
+}
+
+function manageOrderbook {
+    # Acquire the properly-parsed orderbook (first argument) and produce filtered sub-books
+    filterOrderbook $1 'renewals' | renewLease # pipe renewals to own subroutine
+    filterOrderbook $1 'starts' | startLease # pipe new leases to own subroutine
+    filterOrderbook $1 'old' | endLease # pipe old leases to own subroutine (for dismission)
 
 }
 
@@ -17,19 +42,22 @@ function startLease {
 
 }
 
-function endLease {
-
-}
 
 function renewLease {
 
 }
 
+function endLease {
+
+}
+
+function removeOld {
+
+}
+
 function main {
   acquireRequests
-  renewLease
-  endLease
-  startLease
+  manageOrderbook
 }
 
 main #> /dev/null 2>&1
